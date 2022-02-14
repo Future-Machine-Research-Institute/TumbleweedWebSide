a2af60f4c3f3ebe6ffef2a340829b70312939f4c
arrosev <1149797129@qq.com>
2022-02-14 23:33:40 +0800

    引入ElementPlus和环境配置

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <Login />
 </template>
 
 <style>
+
+html,body,#app{
+  width: 100%;
+  height: 100%;
+  margin: auto;
+  box-sizing: content-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
+
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <el-button>Default</el-button>
+</template>
+
+<script>
+    // import { ElButton } from 'element-plus'
+    export default {
+        data() {
+            return {
+                // greeting: 'Hello World!!!!!'
+            }
+        },
+        //生命周期 - 创建完成,访问当前this实例
+        created() {
+            
+        },
+        //生命周期 - 挂载完成,访问DOM元素
+        mounted() {
+            
+        }
+    }
+</script>
+
+<style scoped>
+/* @import url(); 引入css类 */
+
+
+    
+</style>
