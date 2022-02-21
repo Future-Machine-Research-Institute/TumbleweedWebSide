457a6228c0e9d07263fa3e0dd2c91e5ba043cb12
arrosev <1149797129@qq.com>
2022-02-21 22:18:25 +0800

    项目目录调整

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Login from './components/Login.vue';
-import Home from './components/Home.vue'
+import Login from './views/Login.vue';
+import Home from './views/Home.vue';
 
 // export default {
 //   components: {
