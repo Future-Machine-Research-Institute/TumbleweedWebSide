00545da7dc6624ee9bc35302ce6fdf56aeaa8e9d
Arrose <arrose.chen@foxmail.com>
2022-09-22 23:09:57 +0800

    app update

--- src/views/AppManagement/AppManagementContainer.vue
+++ src/views/AppManagement/AppManagementContainer.vue
@@ -9,7 +9,7 @@
       <div class="upload-dialog-body">
         <el-form class="upload-dialog-form" ref = "uploadPackageForm" :model = "uploadPackageForm" :rules = "uploadPackageRules">
           <el-form-item label="App文件" prop = "fileName">
-            <el-input v-model="uploadPackageForm.fileName" autocomplete="off" type="file" ref="fileSelectInput" accept=".apk,.ipa" @change="fileSelect" />
+            <el-input v-model="uploadPackageForm.fileName" autocomplete="off" type="file" ref="uploadFileSelectInput" accept=".apk,.ipa" @change="uploadFileSelect" />
           </el-form-item>
           <el-form-item label="App名称" prop = "appName">
             <el-input v-model="uploadPackageForm.appName" />
@@ -43,6 +43,50 @@
       </template>
     </el-dialog>
 
+    <el-dialog v-model="updatePackageDialogVisible" title="App包更新" width="100%" top="0px">
+      <div v-if="isShowUpdateProgress" class="update-progress-container">
+        <el-progress class="update-progress" type="circle" :percentage="updatePercentage" />
+      </div>
+      <div class="update-dialog-body">
+        <el-form class="update-dialog-form" ref = "updatePackageForm" :model = "updatePackageForm" :rules = "updatePackageRules">
+          <el-form-item label="AppId" prop = "appId">
+            <el-input v-model="updatePackageForm.appId" />
+          </el-form-item>
+          <el-form-item label="App文件" prop = "fileName">
+            <el-input v-model="updatePackageForm.fileName" autocomplete="off" type="file" ref="updateFileSelectInput" accept=".apk,.ipa" @change="updateFileSelect" />
+          </el-form-item>
+          <el-form-item label="App名称" prop = "appName">
+            <el-input v-model="updatePackageForm.appName" />
+          </el-form-item>
+          <el-form-item label="App版本" prop = "appVersion">
+            <el-input v-model="updatePackageForm.appVersion" />
+          </el-form-item>
+          <el-form-item label="App系统" prop = "appSystem">
+            <el-radio-group v-model="updatePackageForm.appSystem" disabled>
+              <el-radio :label="0">IOS</el-radio>
+              <el-radio :label="1">Android</el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item label="App进度" prop = "appProgress">
+            <el-radio-group v-model="updatePackageForm.appProgress">
+              <el-radio-button :label="0">正式版</el-radio-button>
+              <el-radio-button :label="1">测试版</el-radio-button>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item label="App描述" prop = "appDescription">
+            <el-input v-model="updatePackageForm.appDescription" maxlength="30" show-word-limit type="textarea"/>
+          </el-form-item>
+        </el-form>
+      </div>
+      
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="updatePackageDialogVisible = false">取消</el-button>
+          <el-button type="primary" @click="updatePackage">更新</el-button>
+        </span>
+      </template>
+    </el-dialog>
+
     <el-table class="container" @selection-change="appListTableSelectionChange" id="appListTable" ref="appListTable" v-scroll="appListTableScrollToBottom" :data="appListTableData" max-height="calc(100vh - 400px)" :row-style="{height: '60px'}" v-loading="appListTableLoading">
       <el-table-column type="selection" align = "center"/>
       <el-table-column fixed label="图标" align = "center">
@@ -60,7 +104,9 @@
       </el-table-column>
       <el-table-column prop="uploadAccount" label="上传者" align = "center"/>
       <el-table-column fixed="right" label="Operations" align = "center">
-        <el-button link type="primary" size="small" @click="updatePackage">更新</el-button>
+        <template #default="scope">
+          <el-button link type="primary" size="small" @click="updatePackageDialog(scope.row)">更新</el-button>
+        </template>
       </el-table-column>
       <!-- <template #append>
         <div class="scroll-loading" v-loading="appListTableScrollLoading" style="width: 100%; height: 35px; margin: 0 auto;"></div>
@@ -100,7 +146,7 @@
   import NetApiShareInstance from '../../net/net-api'
   import { errorNotificationShow, successNotificationShow } from '../../utils/notification-view'
   import { parseInt } from 'lodash'
-  import { ref } from 'vue'
+import { onKeyDown } from '@vueuse/core'
   // import { loadingViewShow, loadingViewDismiss} from '../../utils/loading-view'
 
     export default {
@@ -110,9 +156,22 @@
         data() {
             return {
               isShowUploadProgress: false,
+              isShowUpdateProgress: false,
               uploadPercentage: 0,
+              updatePercentage: 0,
               uploadPackageDialogVisible: false,
+              updatePackageDialogVisible: false,
               uploadPackageForm: {
+                fileName: "",
+                appName: "",
+                appVersion: "",
+                appSystem: null,
+                appProgress: null,
+                appDescription: "",
+                appIcon: ""
+              },
+              updatePackageForm: {
+                appId: "",
                 fileName: "",
                 appName: "",
                 appVersion: "",
@@ -129,7 +188,8 @@
               isAppListLoadFinished: false,
               obtainedCount: 0,
               appListTableData:[],
-              appListTableSelectionData: []
+              appListTableSelectionData: [],
+              appUpdatePackageData:null
             }
         },
         directives: {
@@ -203,13 +263,59 @@
               ],
             }
           },
+          updatePackageRules() {
+            return {
+              appId: [
+                {
+                  required: true,
+                  message: "AppId不能为空"
+                },
+              ],
+              fileName: [
+                {
+                  required: true,
+                  message: "上传的文件不能为空"
+                },
+              ],
+              appName: [
+                {
+                  required: true,
+                  message: "App名称不能为空"
+                }
+              ],
+              appVersion: [
+                {
+                  required: true,
+                  message: "App版本不能为空"
+                }
+              ],
+              appSystem: [
+                {
+                  required: true,
+                  message: "App系统未选择"
+                }
+              ],
+              appProgress: [
+                {
+                  required: true,
+                  message: "App进度未选择"
+                }
+              ],
+              appDescription: [
+                {
+                  required: true,
+                  message: "App描述不能为空"
+                }
+              ],
+            }
+          }
         },
         //方法
         methods: {
-            async fileSelect() {
+            async uploadFileSelect() {
               try {
                 //IOS = 0 正式版 = 0
-                const file = this.$refs.fileSelectInput.input.files[0]
+                const file = this.$refs.uploadFileSelectInput.input.files[0]
                 if(file) {
                   const extension = file.name.substr(file.name.lastIndexOf(".") + 1)
                   let apkInfo = await readPkgInfo(file);
@@ -223,12 +329,34 @@
               }
             },
 
+            async updateFileSelect() {
+              try {
+                //IOS = 0 正式版 = 0
+                const file = this.$refs.updateFileSelectInput.input.files[0]
+                if(file) {
+                  const extension = file.name.substr(file.name.lastIndexOf(".") + 1)
+                  if(this.appUpdatePackageData.system === (extension === "ipa" ? 0 : 1)) {
+                    let apkInfo = await readPkgInfo(file);
+                    this.updatePackageForm.appName = apkInfo.name
+                    this.updatePackageForm.appVersion = apkInfo.versionName
+                    this.updatePackageForm.appSystem = extension === "ipa" ? 0 : 1
+                    this.updatePackageForm.appIcon = apkInfo.icon
+                  } else {
+                    this.updatePackageForm.fileName = ""
+                    errorMessageShow("更新包的系统类型必须一致")
+                  }
+                }
+              } catch (error) {
+                errorMessageShow(error)
+              }
+            },
+
             async uploadPackage() {
               this.$refs.uploadPackageForm.validate(async (isok) => {
                 if (isok) {
                   this.isShowUploadProgress = true
                   try {
-                    const file = this.$refs.fileSelectInput.input.files[0]
+                    const file = this.$refs.uploadFileSelectInput.input.files[0]
                     const fileMd5 = await md5FromFile(file)
                     const utcTimeStamp = getUTCTimeStamp()
                     let formData = new FormData()
@@ -277,8 +405,30 @@
               })
             },
 
+            updatePackageDialog(data) {
+
+              this.appUpdatePackageData = data
+              this.updatePackageDialogVisible = true
+              
+              this.updatePackageForm.appId = data.appId
+              this.updatePackageForm.fileName = ""
+              this.updatePackageForm.appName = data.appName
+              this.updatePackageForm.appVersion = data.version
+              this.updatePackageForm.appSystem = data.system
+              this.updatePackageForm.appProgress = data.progress
+              // this.uploadPackageForm.appIcon = data.appIcon
+              // this.updatePackageForm.appDescription = data.appDescription
+            },
+
             updatePackage () {
-              console.log("updatePackage")
+              // console.log("data: ", data)
+              this.$refs.updatePackageForm.validate(async (isok) => {
+                if(isok) {
+                  
+                } else {
+
+                }
+              })
             },
 
             deletePackage() {
@@ -358,7 +508,7 @@
                     this.isAppListLoadFinished = res.data.finished
                     this.scrollLoadingSearchObject = searchObject
                     this.$refs.appListTable.clearSelection()
-                    console.log("删除成功, this.appListTableSelectionData: ", this.appListTableSelectionData)
+                    // console.log("删除成功, this.appListTableSelectionData: ", this.appListTableSelectionData)
                   } else {
                     errorNotificationShow("获取App列表失败", res.data.message)
                   }
@@ -396,26 +546,6 @@
         //生命周期 - 挂载完成,访问DOM元素
         mounted() {
           this.appListTableAllReload(this.requiredCount, 0, {})
-          // if (this.appListTableScrollLoading === true || this.appListTableLoading === true) {
-          //   console.log("正在等待加载.....")
-          // } else {
-          //   this.appListTableLoading = true
-          //   NetApiShareInstance.packageObtain(readAccount(), readToken(), this.requiredCount, 0, {}).then((res) => {
-          //     if (res.data.ret === 0) {
-          //       console.log("list: ", res.data.items)
-          //       console.log("finished: ", res.data.finished)
-          //       this.obtainedCount = this.obtainedCount + res.data.items.length
-          //       this.appListTableData.push.apply(this.appListTableData, res.data.items)
-          //       this.isAppListLoadFinished = res.data.finished
-          //     } else {
-          //       errorNotificationShow("获取App列表失败", res.data.message)
-          //     }
-          //     this.appListTableLoading = false
-          //   }).catch((err) => {
-          //     this.appListTableLoading = false
-          //     errorMessageShow(err)
-          //   })
-          // }
         },
         unmounted() {
           console.log("AppManagerContainer -- delloc")
@@ -478,7 +608,27 @@
   /* background-color: black; */
 }
 
+.update-dialog-body {
+  width: 100%;
+  height: 354px;
+}
+
 .upload-progress-container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 999999;
+  background: rgba(255, 255, 255, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.update-progress-container {
   position: absolute;
   width: 100%;
   height: 100%;
@@ -502,6 +652,11 @@
   align-items: center;
 }
 
+.update-progress {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 /* .scroll-loading :deep(.el-loading-spinner) {
   width: 35px;
   height: 35px;
